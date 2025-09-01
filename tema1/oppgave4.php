1e1eaf8d699fd6b91d6dcf951d30c88e78756634
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-09-01 14:26:18 +0200

    endret filtype

---
+++ tema1/oppgave4.php
@@ -0,0 +1,19 @@
+<?php
+$tall1=$_POST ["tall1"];
+$tall2=$_POST ["tall2"];   
+print("tall 1 er $tall1");
+print("<br />");
+print("tall 2 er $tall2");
+$sum=$tall1+$tall2;
+$diferanse=$tall1-$tall2;
+print("<br />");
+print("summen er $sum");
+print("<br />");
+print("differansen er $diferanse");    
+$gang=$tall1*$tall2;
+$del=$tall1/$tall2;
+print("<br />");
+print("gange er $gang");
+print("<br />");
+print("dele er $del");
+?>
