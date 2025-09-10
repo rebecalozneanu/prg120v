ef1f6e9104e4eaef79441d010f91343fef36fb3c
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-09-10 14:51:27 +0200

    fil

--- tema2/oppgave4.php
+++ tema2/oppgave4.php
@@ -1,32 +1,27 @@
-<?php /* Oppgave 4 */
-/*
-/* Programmet mottar fra et HTML-skjema et svar på spørsmålene
-/* "Er du gift (j/n) ?" og "Har du barn (j/n) ?"
-/* Programmet sjekker om det er svart j eller n på spørsmålet og skriver ut en passende melding
-*/
+<?php 
 $gift=$_POST ["gift"];
 $barn=$_POST ["barn"];
-if (!$gift || !$barn) /* det er ikke svart på begge spørsmålene */
+if (!$gift || !$barn) 
 {
 print("Du har ikke svart p&aring; begge sp&oslash;rsm&aring;lene <br/>");
 }
-else if ($gift == "j" && $barn == "j") /* det er svart j på spørsmålet om gift og j på spørsmålet om barn */
+else if ($gift == "j" && $barn == "j") 
 {
 print("Du er gift og har barn <br/>");
 }
-else if ($gift == "j" && $barn == "n") /* det er svart j på spørsmålet om gift og n på spørsmålet om barn */
+else if ($gift == "j" && $barn == "n") 
 {
 print("Du er gift og har ikke barn <br/>");
 }
-else if ($gift == "n" && $barn == "j") /* det er svart n på spørsmålet om gift og j på spørsmålet om barn */
+else if ($gift == "n" && $barn == "j") 
 {
 print("Du er ikke gift og har barn <br/>");
 }
-else if ($gift == "n" && $barn == "n") /* det er svart n på spørsmålet om gift og n på spørsmålet om barn */
+else if ($gift == "n" && $barn == "n")
 {
 print("Du er ikke gift og har ikke barn <br/>");
 }
-else /* det er ikke svart verken j eller n på begge spørsmålene */
+else 
 {
 print("Du har ikke svart ja eller nei p&aring; begge sp&oslash;rsm&aring;lene <br/>");
 }
