91b88cb875c0a3c06b199be1f1f865aa3c8fc4d9
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-09-10 13:17:39 +0200

    fil

--- tema2/oppgave3.php
+++ tema2/oppgave3.php
@@ -1,20 +1,19 @@
 <?php 
 $svar=$_POST ["svar"];
-if (!$svar) 
+if (!$svar)  
 {
 print("Du har ikke svart p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
 }
-else if ($svar == "j") 
+else if ($svar == "j" || $svar == "J" || $svar == "ja" || $svar == "JA" || $svar == "Ja" )
 {
 print("Du har svart ja p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
 }
-else if ($svar == "n") 
-{
+else if ($svar == "n" || $svar == "N" || $svar == "nei" || $svar == "NEI" || $svar == "Nei")
+{ 
 print("Du har svart nei p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
 }
-else 
-{
+else
+{ 
 print("Du har ikke svart ja eller nei p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
 }
 ?>
-
