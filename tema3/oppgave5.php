27ec44f9cab3b42173e14a3c9796c3ab5c261351
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-09-15 15:35:01 +0200

    fil

--- tema3/oppgave5.php
+++ tema3/oppgave5.php
@@ -5,6 +5,6 @@ for($tall=1;$tall<=10;$tall++);
     $sum= $sum+$tall;
 }
 $gjennomsnitt=$sum/10
-print("Summen av tellene fra 1 til 10 er $sum</br>");
+print("Summen av tallene fra 1 til 10 er $sum</br>");
 print("Gjennomsnittet av tallene 1 til 10 er $gjennomsnitt </br>")
 ?>
