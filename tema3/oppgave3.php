cc23ea18567cf1a41cad309ece0bf6df59a5d1fb
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-09-15 13:49:48 +0200

    fil

--- tema3/oppgave3.php
+++ tema3/oppgave3.php
@@ -1,16 +1,16 @@
 <?php
 for ($tall=1;$tall<=10;$tall++)
 {
-    print($tall );
+    print("$tall ");
 }
 print ("<br>");
 for($tall=11;$tall<=20; $tall++)
 {
-    print($tall );
+    print("$tall ");
 }
 print("<br>");
 for($tall=21; $tall<=30; $tall++)
 {
-    print($tall );
+    print("$tall ");
 }
 ?>
