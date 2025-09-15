d681975d2cda340010bb3ea7c439304c0f02801d
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-09-15 12:32:32 +0200

    fil

---
+++ tema3/oppgave1.php
@@ -0,0 +1,4 @@
+<?<php
+for ($tall=1;$tall<=10;$tall++)
+    print("tall<br/>");
+?>
---
+++ tema3/oppgave2.php
@@ -0,0 +1,5 @@
+<?php
+for("$tall=1;$tall<=10;$tall++")
+    $kvadrat=$tall*$tall
+print("$tall har kvadrat $kvadrat </br>")
+?>
