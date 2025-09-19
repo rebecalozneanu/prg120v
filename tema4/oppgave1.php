0e9997a7c971c9f35dfd8660f57c00308e32f566
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-09-19 13:26:30 +0200

    fil

--- tema4/oppgave1.php
+++ tema4/oppgave1.php
@@ -1,4 +1,16 @@
 <?php
 $postnr=$_POST["postnr"];
+if(!$postnr) {
+    print("Postnr er ikke fylt ut </br/>");
+
+} else if (strlen($postnr)!=4) {//1234 | 4 != 4
+        print("Postnr må være på 4 tall </br>");
+    
+} else if (!ctype_digit($postnr)) {
+    print("Bruke tall")
+}else {
+    print ("$postnr")
+}
+
 print("$postnr");
 ?>
