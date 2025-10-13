ea19d06133648a788533e4584399f293b5c99282
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-10-13 13:31:09 +0200

    endret navn

--- arbeidskrav/slette-klasse.php
+++ arbeidskrav/slette-klasse.php
@@ -1,23 +1,23 @@
-<?php /* slett-emne */
+<?php /* slett-klasse */
 /*
-/* Programmet lager et skjema for å kunne slette et emne
-/* Programmet sletter det valgte emne
+/* Programmet lager et skjema for å kunne slette et klasse
+/* Programmet sletter det valgte klasse
 */
 ?>
 <script src="funksjoner.js"> </script>
-<h3>slette emne</h3>
+<h3>slette klasse</h3>
 <form method="post" action="" id="sletteemneskjema" name="sletteemneskjema" onSubmit="return bekreft()">
 Emnekode <input type="text" id="sletteemne" name="sletteemne" required /> <br/>
-<input type="submit" value="Slett emne" name="sletteemneKnapp" id="sletteemneKnapp" />
+<input type="submit" value="Slett klasse" name="sletteemneKnapp" id="sletteemneKnapp" />
 </form>
 <?php
 if (isset($_POST ["sletteemneKnapp"]))
 {
 include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
 $emnekode=$_POST ["sletteemne"];
-$sqlSetning="DELETE FROM emne WHERE emnekode='$emnekode';";
+$sqlSetning="DELETE FROM klasse WHERE emnekode='$emnekode';";
 mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
 /* SQL-setning sendt til database-serveren */
-print ("F&oslash;lgende emne er n&aring; slettet: $emnekode <br />");
+print ("F&oslash;lgende klasse er n&aring; slettet: $emnekode <br />");
 }
 ?>
