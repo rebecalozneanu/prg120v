95f67de9351adb9dfd7e56a20373fc7fc60d0335
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-10-20 13:41:36 +0200

    fhg

--- arbeidskrav/slette-klasse.php
+++ arbeidskrav/slette-klasse.php
@@ -7,17 +7,17 @@
 <script src="funksjoner.js"> </script>
 <h3>slette klasse</h3>
 <form method="post" action="" id="sletteemneskjema" name="sletteemneskjema" onSubmit="return bekreft()">
-Emnekode <input type="text" id="sletteklasse" name="sletteklasse" required /> <br/>
+klassekode <input type="text" id="sletteklasse" name="sletteklasse" required /> <br/>
 <input type="submit" value="Slett klasse" name="sletteemneKnapp" id="sletteemneKnapp" />
 </form>
 <?php
 if (isset($_POST ["sletteemneKnapp"]))
 {
 include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
-$emnekode=$_POST ["sletteklasse"];
-$sqlSetning="DELETE FROM klasse WHERE emnekode='$emnekode';";
+$klassekode=$_POST ["sletteklasse"];
+$sqlSetning="DELETE FROM klasse WHERE klassekode='$klassekode';";
 mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
 /* SQL-setning sendt til database-serveren */
-print ("F&oslash;lgende klasse er n&aring; slettet: $emnekode <br />");
+print ("F&oslash;lgende klasse er n&aring; slettet: $klassekode <br />");
 }
 ?>
