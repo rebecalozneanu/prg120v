b7e9844559ffa79c457652152a6feb2de810d3d1
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-10-20 13:38:19 +0200

    g

--- arbeidskrav/vis-alle-studenter.php
+++ arbeidskrav/vis-alle-studenter.php
@@ -9,15 +9,15 @@ $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente dat
 $antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
 print ("<h3>Registrerte studenter </h3>");
 print ("<table border=1>");
-print ("<tr><th align=left>brukenavn</th> <th align=left>fornavn</th><th align=left>etternavn</th><th align=left>klassekode</th> </tr>");
+print ("<tr><th align=left>brukernavn</th> <th align=left>fornavn</th><th align=left>etternavn</th><th align=left>klassekode</th> </tr>");
 for ($r=1;$r<=$antallRader;$r++)
 {
 $rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
-$brukenavn=$rad["brukenavn"];
+$brukernavn=$rad["brukernavn"];
 $fornavn=$rad["fornavn"];
 $etternavn=$rad["etternavn"];
 $klassekode=$rad["klassekode"];
-print ("<tr><td> $brukenavn </td> <td> $fornavn </td> <td> $etternavn </td><td> $klassekode </td></tr>");
+print ("<tr><td> $brukernavn </td> <td> $fornavn </td> <td> $etternavn </td><td> $klassekode </td></tr>");
 }
 print ("</table>");
 ?>
