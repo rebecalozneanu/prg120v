3acae2c5902ba8192291eb77e2df09b96c85c5a2
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-10-20 13:45:31 +0200

    lkj

--- arbeidskrav/slette-student.php
+++ arbeidskrav/slette-student.php
@@ -7,17 +7,17 @@
 <script src="funksjoner.js"> </script>
 <h3>slette student</h3>
 <form method="post" action="" id="sletteStudentSkjema" name="sletteStudentSkjema" onSubmit="return bekreft()">
-Studentkode <input type="text" id="studentkode" name="studentkode" required /> <br/>
+brukernavn$brukernavn <input type="text" id="brukernavn$brukernavn" name="brukernavn$brukernavn" required /> <br/>
 <input type="submit" value="Slette student" name="sletteStudentKnapp" id="sletteStudentKnapp" />
 </form>
 <?php
 if (isset($_POST ["sletteStudentKnapp"]))
 {
 include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
-$studentkode=$_POST ["studentkode"];
-$sqlSetning="DELETE FROM student WHERE studentkode='$studentkode';";
+$brukernavn=$_POST ["brukernavn$brukernavn"];
+$sqlSetning="DELETE FROM student WHERE brukernavn='$brukernavn';";
 mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
 /* SQL-setning sendt til database-serveren */
-print ("F&oslash;lgende student er n&aring; slettet: $studentkode <br />");
+print ("F&oslash;lgende student er n&aring; slettet: $brukernavn <br />");
 }
 ?>
