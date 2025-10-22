0d3e0568de996a9a13e261f39a956b34c9ff8285
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-10-22 13:52:48 +0200

    fijm

--- arbeidskrav/slette-klasse.php
+++ arbeidskrav/slette-klasse.php
@@ -1,23 +1,65 @@
-<?php /* slett-klasse */
+<?php  /* slett-klasse */
 /*
-/* Programmet lager et skjema for å kunne slette et klasse
-/* Programmet sletter det valgte klasse
-*/
+/*  Programmet lager et skjema for å velge en klasse som skal slettes
+/*  Programmet sjekker om det er studenter i klassen før sletting
+/*  Programmet sletter den valgte klassen */
 ?>
+
 <script src="funksjoner.js"> </script>
-<h3>slette klasse</h3>
-<form method="post" action="" id="sletteemneskjema" name="sletteemneskjema" onSubmit="return bekreft()">
-klassekode <input type="text" id="sletteklasse" name="sletteklasse" required /> <br/>
-<input type="submit" value="Slett klasse" name="sletteemneKnapp" id="sletteemneKnapp" />
+
+<h3>Slett klasse</h3>
+
+<form method="post" action="" id="slettKlasseSkjema" name="slettKlasseSkjema" onSubmit="return bekreft()">
+  Klasse <br />
+  <?php
+    // Dynamisk listeboks
+    include("db-tilkobling.php");
+
+    echo '<select name="klassekode" id="klassekode">';
+    echo '<option value=""> Velg klasse </option>';
+
+    $sql = "SELECT klassekode, klassenavn FROM klasse ORDER BY klassekode;";
+    $res = mysqli_query($db, $sql) or die("Feil ved henting av klasser fra databasen");
+    while ($rad = mysqli_fetch_assoc($res)) {
+        $kode = htmlspecialchars($rad['klassekode'], ENT_QUOTES, 'UTF-8');
+        $navn = htmlspecialchars($rad['klassenavn'], ENT_QUOTES, 'UTF-8');
+        echo "<option value=\"{$kode}\">{$kode} - {$navn}</option>";
+    }
+
+    echo '</select>';
+  ?> <br/>
+  <input type="submit" value="Slett klasse" name="slettKlasseKnapp" id="slettKlasseKnapp" />
 </form>
+
 <?php
-if (isset($_POST ["sletteemneKnapp"]))
-{
-include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
-$klassekode=$_POST ["sletteklasse"];
-$sqlSetning="DELETE FROM klasse WHERE klassekode='$klassekode';";
-mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
-/* SQL-setning sendt til database-serveren */
-print ("F&oslash;lgende klasse er n&aring; slettet: $klassekode <br />");
-}
+  if (isset($_POST["slettKlasseKnapp"]))
+    {
+      $klassekode = isset($_POST["klassekode"]) ? $_POST["klassekode"] : '';
+      if ($klassekode === '')
+        {
+          print ("Ingen klasse er valgt. <br />");
+        }
+      else
+        {
+          include("db-tilkobling.php");
+
+          $kk = mysqli_real_escape_string($db, $klassekode);
+
+          /* Sjekk om det finnes studenter i klassen */
+          $sjekkSql = "SELECT COUNT(*) AS cnt FROM student WHERE klassekode = '$kk';";
+          $sjekkResult = mysqli_query($db, $sjekkSql) or die("Feil ved sjekk i databasen");
+          $row = mysqli_fetch_assoc($sjekkResult);
+
+          if ($row['cnt'] > 0)
+            {
+              print ("Du kan ikke slette en klasse som har studenter i seg (klassekode: $kk) <br />");
+            }
+          else
+            {
+              $sqlSetning = "DELETE FROM klasse WHERE klassekode='$kk';";
+              mysqli_query($db, $sqlSetning) or die ("Ikke mulig å slette data i databasen");
+              print ("Valgt klasse er nå slettet. <br />");
+            }
+        }
+    }
 ?>
--- arbeidskrav/slette-student.php
+++ arbeidskrav/slette-student.php
@@ -1,23 +1,51 @@
-<?php /* slette-student */
+<?php  /* slett-student */
 /*
-/* Programmet lager et skjema for å kunne slette et student
-/* Programmet sletter det valgte studiet
-*/
+/*  Programmet lager et skjema for å velge en student som skal slettes
+/*  Programmet sletter den valgte studenten */
 ?>
+
 <script src="funksjoner.js"> </script>
-<h3>slette student</h3>
-<form method="post" action="" id="sletteStudentSkjema" name="sletteStudentSkjema" onSubmit="return bekreft()">
-brukernavn <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
-<input type="submit" value="Slette student" name="sletteStudentKnapp" id="sletteStudentKnapp" />
+
+<h3>Slett student</h3>
+
+<form method="post" action="" id="slettStudentSkjema" name="slettStudentSkjema" onSubmit="return bekreft()">
+  Student <br />
+  <?php
+    // Dynamisk listeboks
+    include("db-tilkobling.php");
+
+    echo '<select name="brukernavn" id="brukernavn">';
+    echo '<option value=""> Velg student </option>';
+
+    $sql = "SELECT brukernavn, fornavn, etternavn FROM student ORDER BY etternavn, fornavn;";
+    $res = mysqli_query($db, $sql) or die("Feil ved henting av studenter fra databasen");
+    while ($rad = mysqli_fetch_assoc($res)) {
+        $brukernavn = htmlspecialchars($rad['brukernavn'], ENT_QUOTES, 'UTF-8');
+        $navn = htmlspecialchars(trim($rad['fornavn'] . ' ' . $rad['etternavn']), ENT_QUOTES, 'UTF-8');
+        echo "<option value=\"{$brukernavn}\">{$navn} ({$brukernavn})</option>";
+    }
+
+    echo '</select>';
+  ?> <br/>
+  <input type="submit" value="Slett student" name="slettStudentKnapp" id="slettStudentKnapp" />
 </form>
+
 <?php
-if (isset($_POST ["sletteStudentKnapp"]))
-{
-include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
-$brukernavn=$_POST ["brukernavn"];
-$sqlSetning="DELETE FROM student WHERE brukernavn='$brukernavn';";
-mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
-/* SQL-setning sendt til database-serveren */
-print ("F&oslash;lgende student er n&aring; slettet: $brukernavn <br />");
-}
+  if (isset($_POST["slettStudentKnapp"]))
+    {
+      $brukernavn = isset($_POST["brukernavn"]) ? $_POST["brukernavn"] : '';
+      if ($brukernavn === '')
+        {
+          print ("Ingen student er valgt. <br />");
+        }
+      else
+        {
+          include("db-tilkobling.php");
+
+          $bn = mysqli_real_escape_string($db, $brukernavn);
+          $sqlSetning = "DELETE FROM student WHERE brukernavn='$bn';";
+          mysqli_query($db, $sqlSetning) or die ("Ikke mulig å slette data i databasen");
+          print ("Valgt student er nå slettet. <br />");
+        }
+    }
 ?>
