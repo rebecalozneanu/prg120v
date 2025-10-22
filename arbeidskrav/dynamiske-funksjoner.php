bbd1b4a86d13b70bba09ccfef02d30d4be5410fe
rebecalozneanu <rebeca.lozneanu@gmail.com>
2025-10-22 13:50:55 +0200

    fil

--- arbeidskrav/dynamiske-funksjoner.php
+++ arbeidskrav/dynamiske-funksjoner.php
@@ -1,27 +1,48 @@
 <?php  /*  dynamiske funksjoner */
 /*
 /*  denne filen inneholder følgende dynamiske funksjoner:
-/*    listeboksstudent()
-/*    sjekkbokserPostnr()
+/*    listeboksStudent()
+/*    listeboksKlasse()
 */
 
-
-function listeboksstudent()
+function listeboksStudent()
 {
   include("db-tilkobling.php");  /* tilkobling til database-server og valg av database utført */
       
   $sqlSetning="SELECT * FROM student ORDER BY brukernavn;";
-  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen"); 
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");  
     /* SQL-setning sendt til database-serveren */
-	
+      
   $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
 
   for ($r=1;$r<=$antallRader;$r++)
     {
       $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
-      $brukernavn=$rad["brukenavn"]; 
-      
+      $brukernavn=$rad["brukernavn"];       
 
-      print("<option value='$brukernavn'>$ $brukernavn</option>");  /* ny verdi i listeboksen laget */
+      print("<option value='$brukernavn'>$brukernavn</option>");
+        /* ny verdi i listeboksen laget */
     }
 }
+
+function listevboksKlasse()
+{
+  include("db-tilkobling.php");  /* tilkobling til database-server og valg av database utført */
+      
+  $sqlSetning="SELECT * FROM klasse ORDER BY klassekode;";
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");  
+    /* SQL-setning sendt til database-serveren */
+      
+  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
+
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
+      $klassekode=$rad["klassekode"];       
+
+      print("<option value='$klassekode'>$klassekode</option>");
+        /* ny verdi i listeboksen laget */
+    }
+}
+
+?>
